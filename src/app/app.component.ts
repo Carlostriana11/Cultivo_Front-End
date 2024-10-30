960471a79aa02814254242af2cf48df5ff226f06
Carlos Triana <carlostriana404@gmail.com>
2024-10-29 20:11:29 -0500

    The website was completed with a footer uniquely designed and styled for it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NevegationBarComponent } from "./components/public/nevegation-bar/nevegation-bar.component";
+import { FooterComponent } from "./components/public/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NevegationBarComponent],
+  imports: [RouterOutlet, NevegationBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
